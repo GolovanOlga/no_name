385115038077fb7cd93604c3dd33bbf2d57fb5ea
GolovanOlga <olenenok1000@yandex.ru>
2016-04-25 22:11:43 +0300

    2.6

--- module2/2.6/1./1.c
+++ module2/2.6/1./1.c
@@ -1,36 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main () {
+#include <string.h>
+int main (int argc, char *argv[]) {
 FILE *fp;
 struct text {
-char line[100];
+char line[30];
 };
-struct text text_info[1000];
-if ((fp=fopen("read.txt", "r"))==NULL) {
+int u, N;
+u=sizeof(struct text);
+scanf ("%d", &N); //N - число слов в тексте
+N=N*u;
+struct text text_info[N];
+if (argc !=2) {
+	printf("File: %s\n", argv[0]);
+}
+if ((fp=fopen(argv[1], "r"))==NULL) {
 	printf("Ошибка при открытии файла\n");
 	exit(1);
 }
 int i=0, k,n, m=1, t=0, T;
-while (fscanf(fp, "%s", text_info[i].line)!=EOF) {
+// while делит текст на слова, для каждого слова создаётся массив
+while (fscanf(fp, "%s", text_info[i].line)!=EOF) { 
 	i++;
 }
 fclose(fp);
 k=i;
 int B[k];
+/*B[k] - массив, элементами которого являются значения
+соответствующие кол-ву встречаемости слов в тексте*/
 i=0;
+/*while удаляет знаки препинания из строки*/
+while(i<k) {
+	while(text_info[i].line[t]!='\0') {
+		if (text_info[i].line[t]==33 || text_info[i].line[t]==34 || text_info[i].line[t]== 39 || text_info[i].line[t]==44 || text_info[i].line[t]==45 || text_info[i].line[t]==46 || text_info[i].line[t]==58 || text_info[i].line[t]==59 || text_info[i].line[t]==63) {
+			T=t;
+			while (text_info[i].line[t]!='\0') {
+				text_info[i].line[t]=text_info[i].line[t+1];
+				t++;
+			}
+			t=T+1;
+		}
+		else {t++;}
+	}
+	t=0;
+	i++;
+}
+i=0;
+/*while сравнивает слова и записывает в массив B сколько 
+раз слово встретится в тексте*/
 while(i<k) {
 	n=i+1;
 	while(n<k) {
-		T=0;
-		while(t<100) {
-			if(text_info[i].line[t]==text_info[n].line[t]) {
-				T++;
-				t++;
-			}
-			else {t++;}
-		}
-		t=0;
-		if(T==100) {
+		if (strcmp(text_info[i].line, text_info[n].line)==0) {
 			m++;
 			n++;
 		}
@@ -41,19 +62,12 @@ while(i<k) {
 	i++;
 }
 i=0;
+/*если слово уже встречалось в тексте, то while зануляет
+соответствующее значение в массиве B */
 while(i<k) {
 	n=i+1;
 	while(n<k) {
-		T=0;
-		while(t<100) {
-			if(text_info[i].line[t]==text_info[n].line[t]) {
-				T++;
-				t++;
-			}
-			else {t++;}
-		}
-		t=0;
-		if(T==100) {
+		if (strcmp(text_info[i].line, text_info[n].line)==0) {
 			B[n]=0;
 			n++;
 		}
@@ -67,16 +81,21 @@ if((fp=fopen("results.txt", "w"))==NULL) {
 	printf("Ошибка при открытии файла\n");
 	exit(1);
 }
+/*вывод в файл слов и их количества в тексте*/
 while(k>0) {
 	while(i<n) {
 		if(B[i]==k) {
-			fprintf(fp, "%s\n", text_info[i].line);
+			if(text_info[i].line[0]!='\0'){
+			fprintf(fp, "%s  %d\n", text_info[i].line, B[i]);}
 			i++;
 		}
 		else {i++;}
 	}
 	i=0;
 	k--;
+}
+fclose(fp);
+return 0;
 }
 fclose(fp);
 return 0;
--- module2/2.6/2./2.c
+++ module2/2.6/2./2.c
@@ -1,42 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main () {
+int main (int argc, char *argv[]) {
 	FILE *in, *out;
-	char ch, a='A';
-	int c=0;
-	if ((in=fopen("input.txt", "r"))==NULL) {
+	char ch;
+	char a[3]={'1', '0', '1'};
+	int i=0, c=0;
+	if (argc !=2) {
+	    printf("File: %s\n", argv[0]);
+    }
+    if ((in=fopen(argv[1], "r"))==NULL) {
 	    printf("Ошибка при открытии файла\n");
 	    exit(1);
-    } 
+    }
     if ((out=fopen("output.txt", "w"))==NULL) {
 	    printf("Ошибка при открытии файла\n");
 	    exit(1);
     } 
     while(!feof(in)) {
     	ch=getc(in);
-    	if(!feof(in)) {
-    		ch^=a;
+    		if(!feof(in)) {
+    		if(i==3) 
+    			i=0;
+    		ch^=a[i];
+    		i++;
     		putc(ch, out);
-    		c++;
     	}
     }	
     fclose(in);
     fclose(out);
-    if ((in=fopen("output.txt", "r"))==NULL) {
-	    printf("Ошибка при открытии файла\n");
-	    exit(1);
-    } 
-    if ((out=fopen("output2.txt", "w"))==NULL) {
-	    printf("Ошибка при открытии файла\n");
-	    exit(1);
-    } 
-    while(!feof(in) && c>0) {
-    	ch=getc(in);
-    	if(!feof(in)) {
-    		ch^=a;
-    		putc(ch, out);
-    		c--;
-    	} 
-    }
     return 0;
 }
+
